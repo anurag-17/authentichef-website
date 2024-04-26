4c4b436ed92686033e98cb0610a172253c9e1945
Harsh <harshalpawar11@gmail.com>
2024-04-26 20:57:47 +0530

    add

--- server/server.js
+++ server/server.js
@@ -1,82 +1,15 @@
-// const express = require('express');
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// require('dotenv').config();
-// const {notFoundMiddleware}=require("../server/middleware/notfoundmiddleware")
-// const connectDB = require("./Utils/db");
-// const path = require('path');
-
-// const corsOptions = {
-//   origin: ["http://localhost:3000", "http://18.130.221.119:3000", "http://localhost:4000", 
-//   "*"],
-
-//   credentials: true,
-// };
-
-// const server = express();
-
-// server.use(express.json({ limit: "50mb" }));
-// server.use(express.urlencoded({ limit: "500kb", extended: true }));
-// server.use(bodyParser.urlencoded({ extended: true }));
-// server.use(cors(corsOptions));
-
-// connectDB();
-
-// // Showing Api is Running or not //
-
-// server.get("/", (req, res) => {
-//   res.send("API is Running");
-
-// });
-//   // Express routes here
-//   server.use('/api/auth', require("./Route/AuthRouter"));
-//   server.use('/api/chef', require("./Route/ChefRouter"));
-//   server.use('/api/menu', require("./Route/MenuRouter"));
-//   server.use('/api/cuisines', require("./Route/CategoryRouter"));
-//   server.use('/api/dietary', require("./Route/dietaryRouter"));
-//   server.use('/api/DishType', require("./Route/DishTypeRouter"));
-//   server.use('/api/SpiceLevel', require("./Route/SpiceRouter"));
-//   server.use('/api/Orders', require("./Route/cartRoutes"));
-//   server.use('/api/order',require("./Route/OrderRouter"))
-
-//   if (process.env.NODE_ENV === "dev") {
-//     server.use(express.static(path.join(__dirname, 'client', 'build')));
-  
-//     // Define route to serve React app
-//     server.get('*', (req, res) => {
-//       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-//     });
-//   } else {
-//     server.get("/", (req, res) => {
-//       res.send("API is running..");
-//     });
-//   }
-
-//     // Not found Middleware
-
-//     server.use(notFoundMiddleware)
-
-// const PORT = process.env.PORT || 4000;
-// server.listen(PORT, '0.0.0.0', (err) => {
-//   if (err) throw err;
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
-
-
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
- const {notFoundMiddleware}=require("../server/middleware/notfoundmiddleware")
+const { notFoundMiddleware } = require("../server/middleware/notfoundmiddleware");
 const connectDB = require('./Utils/db');
 const path = require('path');
 
 const corsOptions = {
-  origin: ['http://18.130.221.119:3000','http://18.130.221.119:3001', '*'],
+  origin: '*',
   credentials: true,
 };
-console.log('Origin:', corsOptions.origin);
 
 const server = express();
 
@@ -88,7 +21,6 @@ server.use(cors(corsOptions));
 connectDB();
 
 // Showing Api is Running or not //
-
 server.get('/', (req, res) => {
   res.send('API is Running');
 });
