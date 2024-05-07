53d13ce28c61e6264f75ef49f07094977b6e318d
hariomp9 <hariompatil9@gmail.com>
2024-05-07 19:18:37 +0530

    update

--- server/config/googleconfig.js
+++ server/config/googleconfig.js
@@ -8,7 +8,7 @@ module.exports = function (passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: 'http://localhost:4000/api/auth/google/callback',
+        callbackURL: 'http://13.43.174.21:4000/api/auth/google/callback',
     }, async (accessToken, refreshToken, profile, done) => {
         try {
     
