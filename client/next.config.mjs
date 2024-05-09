33d8c5a91cc28277b20a8006b6d2cb2c56596129
Harsh <harshalpawar11@gmail.com>
2024-05-09 15:55:21 +0530

    add

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-      HOSTNAME: '0.0.0.0'
-    }
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+
+export default nextConfig;
