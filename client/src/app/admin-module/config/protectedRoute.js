7c6ea65ba87d39f6c1d78f7bb666fcfb2b39fb42
hariomp9 <hariompatil9@gmail.com>
2024-05-14 10:24:41 +0530

    update

--- client/src/app/admin-module/config/protectedRoute.js
+++ client/src/app/admin-module/config/protectedRoute.js
@@ -2,51 +2,38 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
-
 import axios from "axios";
 import Loader from "../components/admin/loader/Index";
-import { setToken, removeToken, adDetails } from "../../redux/slice";
-
-// import Loader from '../loader';
+import { removeToken } from "../../redux/slice";
 
 const protectedRoute = (WrappedComponent) => {
   const Wrapper = (props) => {
     const router = useRouter();
-    //const dispatch = useDispatch();
-    const { token, isLoggedIn } = useSelector((state) => state.auth);
-    // const loading = true
+    const userAuthToken = useSelector((state) => state.auth?.token);
     const [isLoading, setIsLoading] = useState(false);
     const [isAuth, setIsAuth] = useState(false);
-    // console.log(token);
 
     useEffect(() => {
-      const checkAuth = () => {
-        if (!isLoggedIn && !token) {
-          router.push("http://13.43.174.21:4000/api/admin-module/admin/sign-in");
+      const checkAuth = async () => {
+        if (!userAuthToken) {
+          router.push("/admin-module/admin/sign-in");
+          return;
         }
-        if (token) {
-          verify();
-        }
+        verify();
       };
-
       checkAuth();
-    }, [token, isLoggedIn, router]);
+    }, [userAuthToken, router]);
 
     const verify = async () => {
       setIsLoading(true);
       setIsAuth(false);
       try {
-        const res = await axios.get(`http://13.43.174.21:4000/api/auth/verifyUserToken/${token}`);
-        console.log(res);
-        if (res?.data?.data === null) {
-          router.push("/admin-module/admin/sign-in");
-          dispatch(removeToken());
-        }
+        const res = await axios.get(
+          `http://13.43.174.21:4000/api/auth/verifyUserToken/${userAuthToken}`
+        );
         if (res.status === 200) {
           setIsAuth(true);
           setIsLoading(false);
-          router.push("/admin-module/admin/sign-in");
-          return;
         } else {
           dispatch(removeToken());
           router.push("/admin-module/admin/sign-in");
@@ -63,9 +50,9 @@ const protectedRoute = (WrappedComponent) => {
       <>
         {isLoading ? (
           <Loader />
-        ) : token && isAuth ? (
-          <WrappedComponent {...props} />
-        ) : null}
+        ) : (
+          userAuthToken && isAuth && <WrappedComponent {...props} />
+        )}
       </>
     );
   };
