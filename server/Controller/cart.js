b25b88a46968b91fd2d0f2b7a9754d9a8d14a3fc
Harsh <harshalpawar11@gmail.com>
2024-06-04 10:54:24 +0530

    modify add to cart

--- server/Controller/cart.js
+++ server/Controller/cart.js
@@ -2,88 +2,119 @@ const Cart = require("../Model/cart");
 const MenuItem = require("../Model/MenuItem");
 const User = require("../Model/User");
 
+
+// Working code on Production
 // exports.addToCart = async (req, res, next) => {
-//     try {
-//         const { menuItem, quantity, customization} = req.body;
-        
-//         // Access user ID from req.user
-//         const userId = req.user ? req.user._id : null;
+//   try {
+//     const { items } = req.body;
 
-//         if (!userId) {
-//             // If user is not provided, store the cart data in local storage
-//             const cartData = {
-//                 menuItem,
-//                 quantity,
-//                 customization,
-//             };
-//             // Store cart data in local storage
-//    
 
-//             // Retrieve the menu item corresponding to cartData.menuItem
-//             const menuItems = await MenuItem.findOne({ _id: cartData.menuItem }).select("name price");
+//     const userId = req.user ? req.user._id : null;
 
-//             // Make a empty cart object to store the cart data in array of local storage
-//             return res.status(200).json({ message: "Menu item added to cart", menuItem: menuItems, cartData});
-//         }
-
-//         // Find the user's existing cart or create a new one
-//         let userCart = await Cart.findOne({ user: userId, status: "pending" });
-//         if (!userCart) {
-//             userCart = new Cart({
-//                 user: userId,
-//                 items: []
-//             });
-//         }
-
-//         // Add the Item to the cart
-//         userCart.items.push({ menuItem, quantity, customization });
-//         await userCart.save();
-
-//         // Populate the menuItem field with name and price
-//         const userCard = await Cart.findById(userCart._id).populate({
-//             path: "items.menuItem",
-//             select: "name price"
-//         });
-
-//         res.status(201).json({ message: "Item added to cart", userCard });
-
-//     } catch (error) {
-//         next(error);
+//     if (!items || !Array.isArray(items) || items.length === 0) {
+//       return res.status(400).json({ message: "Items are required and should be an array" });
 //     }
+//     let totalCost = 0;
+
+//     for (const item of items) {
+//       if (!item.menuItem || !item.quantity ) {
+//         return res.status(400).json({ message: "Each item must include a menuItem and a quantity" });
+//       }
+
+//       // Check if the menu item exists
+      
+//       const menuItemExists = await MenuItem.findById(item.menuItem).select("name price");
+//       if (!menuItemExists) {
+//         return res.status(404).json({ message: `Menu item not found: ${item.menuItem}` });
+//       }
+
+//       totalCost += menuItemExists.price * item.quantity;
+//     }
+
+//     console.log("Total cost is", totalCost);
+
+//     // Add the shipping cart logic
+//     let shippingCost = totalCost > 55 ? 0 : 5.99;
+
+    
+
+//     if (!userId) {
+//       // For guest users
+//       const cartData = items.map(item => ({
+//         menuItem: item.menuItem,
+//         quantity: item.quantity,
+//         customization: item.customization,
+//       }));
+
+//       return res.status(200).json({ message: "Items added to cart", cartData });
+//     }
+
+//     let userCart = await Cart.findOne({ user: userId });
+//     if (!userCart) {
+//       userCart = new Cart({
+//         user: userId,
+//         items: [],
+//         Shipping_cost:0
+//       });
+//     }
+
+//     for (const item of items) {
+//       const existingItemIndex = userCart.items.findIndex(cartItem => cartItem.menuItem.toString() === item.menuItem);
+
+//       if (existingItemIndex !== -1) {
+//         // Update quantity of existing item
+//         userCart.items[existingItemIndex].quantity += item.quantity;
+//       } else {
+//         userCart.items.push({ menuItem: item.menuItem, quantity: item.quantity, customization: item.customization });
+//       }
+//     }
+
+//     userCart.Shipping_cost = shippingCost;
+
+//     await userCart.save();
+
+//     // Populate the menuItem field with name and price
+//     const updatedCart = await Cart.findById(userCart._id).populate({
+//       path: "items.menuItem",
+//       select: "name price"
+//     });
+
+//     res.status(201).json({ message: "Items added to cart", updatedCart , shippingCost});
+//   } catch (error) {
+//     next(error);
+//   }
 // }
+
 exports.addToCart = async (req, res, next) => {
   try {
     const { items } = req.body;
-
-
     const userId = req.user ? req.user._id : null;
 
     if (!items || !Array.isArray(items) || items.length === 0) {
       return res.status(400).json({ message: "Items are required and should be an array" });
     }
+
+    // Fetch all menu items and calculate the total cost
     let totalCost = 0;
+    const menuItemMap = new Map();
 
     for (const item of items) {
-      if (!item.menuItem || !item.quantity ) {
+      if (!item.menuItem || !item.quantity) {
         return res.status(400).json({ message: "Each item must include a menuItem and a quantity" });
       }
 
       // Check if the menu item exists
-      
-      const menuItemExists = await MenuItem.findById(item.menuItem).select("name price");
-      if (!menuItemExists) {
+      const menuItem = await MenuItem.findById(item.menuItem).select("name price");
+      if (!menuItem) {
         return res.status(404).json({ message: `Menu item not found: ${item.menuItem}` });
       }
 
-      totalCost += menuItemExists.price * item.quantity;
+      menuItemMap.set(item.menuItem, menuItem);
+      totalCost += menuItem.price * item.quantity;
     }
 
-    console.log("Total cost is", totalCost);
-
-    // Add the shipping cart logic
-    let shippingCost = totalCost > 55 ? 0 : 5.99;
-
-    
+    // Calculate shipping cost based on total cost
+    const shippingCost = totalCost > 55 ? 0 : 5.99;
 
     if (!userId) {
       // For guest users
@@ -93,18 +124,20 @@ exports.addToCart = async (req, res, next) => {
         customization: item.customization,
       }));
 
-      return res.status(200).json({ message: "Items added to cart", cartData });
+      return res.status(200).json({ message: "Items added to cart", cartData, shippingCost });
     }
 
+    // For logged-in users
     let userCart = await Cart.findOne({ user: userId });
     if (!userCart) {
       userCart = new Cart({
         user: userId,
         items: [],
-        Shipping_cost:0
+        shippingCost: 0,
       });
     }
 
+    // Update cart items and recalculate total cost
     for (const item of items) {
       const existingItemIndex = userCart.items.findIndex(cartItem => cartItem.menuItem.toString() === item.menuItem);
 
@@ -112,25 +145,32 @@ exports.addToCart = async (req, res, next) => {
         // Update quantity of existing item
         userCart.items[existingItemIndex].quantity += item.quantity;
       } else {
+        // Add new item to cart
         userCart.items.push({ menuItem: item.menuItem, quantity: item.quantity, customization: item.customization });
       }
     }
 
-    userCart.Shipping_cost = shippingCost;
+    // Recalculate total cost based on the updated cart
+    totalCost = userCart.items.reduce((acc, cartItem) => {
+      const menuItem = menuItemMap.get(cartItem.menuItem.toString());
+      return acc + (menuItem.price * cartItem.quantity);
+    }, 0);
 
+    userCart.Shipping_cost = totalCost > 55 ? 0 : 5.99;
     await userCart.save();
 
     // Populate the menuItem field with name and price
     const updatedCart = await Cart.findById(userCart._id).populate({
       path: "items.menuItem",
-      select: "name price"
+      select: "name price",
     });
 
-    res.status(201).json({ message: "Items added to cart", updatedCart , shippingCost});
+    res.status(201).json({ message: "Items added to cart", updatedCart });
   } catch (error) {
     next(error);
   }
-}
+};
+
 
 
 
