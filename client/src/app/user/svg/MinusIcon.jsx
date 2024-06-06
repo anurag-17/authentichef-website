610b7155107e3d3bd543b6cc8d5c56e7a8009a7b
Anurag Pandey <anurag.pandey17@gmail.com>
2024-06-06 11:00:37 +0530

    navbar, rightdrawer

---
+++ client/src/app/user/svg/MinusIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const MinusIcon = () => {
+  return (
+  <>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="sm:size-6 size-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+</svg>
+
+  </>
+  )
+}
