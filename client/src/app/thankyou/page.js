79043757d800a570ba8d6419cc018126512c8de4
shubham21222 <shubhamraikwar@hotmail.com>
2024-06-10 19:42:50 +0530

    wok on checkout

--- client/src/app/thankyou/page.js
+++ client/src/app/thankyou/page.js
@@ -14,7 +14,7 @@ const ThankU = () => {
 
   const pollForSessionId = async () => {
     const pollInterval = 5000; 
-    const maxAttempts = 1; 
+    const maxAttempts = 10; 
 
     for (let attempt = 1; attempt <= maxAttempts; attempt++) {
       try {
