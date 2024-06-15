c4b19d7affb7323754b786f954033341ca74358f
harshal <harshalpawar667@gmail.com>
2024-06-15 11:43:31 +0530

    Add New

--- server/Route/chefjoinRouter.js
+++ server/Route/chefjoinRouter.js
@@ -4,23 +4,23 @@ const {isAuthenticatedUser,AuthorizeRoles} = require('../middleware/auth');
 const { createChef, getChefs, getChefbyId ,updateChef,deleteChef } = require('../Controller/chefjoinController');
 
 // Create a new chef
-router.post('/', isAuthenticatedUser , createChef);
+router.post('/' , createChef);
 
 // Get all chefs
 
-router.get('/', isAuthenticatedUser, getChefs);
+router.get('/',  getChefs);
 
 // Get a single chef
 
-router.get('/:id', isAuthenticatedUser ,getChefbyId);
+router.get('/:id' ,getChefbyId);
 
 // Update a chef
 
-router.put('/:id', isAuthenticatedUser , updateChef);
+router.put('/:id' , updateChef);
 
 // Delte a chef
 
-router.delete('/:id', isAuthenticatedUser ,deleteChef);
+router.delete('/:id' ,deleteChef);
 
 module.exports = router;
 // Compare this snippet from Food-work/server/Route/chefjoinRouter.js:
